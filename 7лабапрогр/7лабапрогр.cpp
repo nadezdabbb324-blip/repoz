f28d89cdf6ab768cee4af064c93acdf437e5f412
nadezdabbb324-blip <nadezdabbb324@gmail.com>
2026-02-26 16:18:39 +1000

    Add files via upload

---
+++ 7лабапрогр/7лабапрогр.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <locale>
+using namespace std;
+
+struct MARSH {
+    string nach;
+    string kon;
+};
+
+int main() {
+    setlocale(LC_ALL, "RUSSIAN");
+    MARSH s[8];
+    for (int i = 0; i < 8; i++) {
+        cout << "Введите начало маршрута номер " << i+1 << ": ";
+        cin >> s[i].nach;
+        cout << "Введите конец маршрута номер " << i+1 << ": ";
+        cin >> s[i].kon;
+    }
+    int n;
+    cout << "Введите номер маршрута: ";
+    cin >> n;
+    if (n <= 8) {
+        cout << "Начало и конец вашего маршрута: " << s[n - 1].nach << ", " << s[n - 1].kon;
+    }
+    else {
+        cout << "Такого маршрута не существует";
+    }
+}
